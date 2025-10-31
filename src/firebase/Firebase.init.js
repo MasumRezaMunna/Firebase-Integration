861200f2a74c3bbcfc9e5b5825fa8e1927c32003
MasumRezaMunna <masumrezamunna2005@gmail.com>
2025-10-31 20:14:16 +0600

    basic project setup

---
+++ src/firebase/Firebase.init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAtailC3PSMFpZ-PyE4JxWNqjKXfZGLzL8",
+  authDomain: "react-firebase-authinteg-69ad1.firebaseapp.com",
+  projectId: "react-firebase-authinteg-69ad1",
+  storageBucket: "react-firebase-authinteg-69ad1.firebasestorage.app",
+  messagingSenderId: "445069246933",
+  appId: "1:445069246933:web:6ce3e38ebd0c5b62e0bd5c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
